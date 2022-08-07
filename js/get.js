43714eaf9420b83b230ebdefac415a7afd8f1934
1817jitu <jitujitu1817@gmail.com>
2022-08-07 08:35:47 +0530

    javascript

---
+++ js/get.js
@@ -0,0 +1,16 @@
+//alert("hello")
+
+
+
+
+
+
+var element;
+
+element=document.getElementById("g").innerText;
+// element=document.getElementById("g").innerHTML;
+// element=document.getElementById("g").getAttribute("style");
+// element=document.getElementById("g").getAttributeNode("style");
+// element=document.getElementById("g").attributes[0];
+
+console.log(element);
